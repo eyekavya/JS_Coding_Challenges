17f6289cc0a8ef083707bf3489c58ca935dbe13a
Kavya <kavya@Shivams-MacBook-Pro.local>
2022-06-29 00:52:01 +0530

    challenge  completed

--- challenges.js
+++ challenges.js
@@ -44,3 +44,23 @@ console.log(markHigherBMI2);
 
 console.log(`Hi!
 I'm Kavya..`);
+
+////////////////////////////////////////
+// Coding Challenge #2
+
+// Use the BMI example from Challenge #1, and the code you already wrote, and
+// improve it.
+// Your tasks:
+// 1. Print a nice output to the console, saying who has the higher BMI. The message
+// is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+// BMI (28.3) is higher than John's (23.9)!"
+// Hint: Use an if/else statement
+
+if (marksBmi1 > johnBmi1) {
+  console.log(`Marks's BMI (${marksBmi1}) is higher than John's (${johnBmi1})`);
+} else {
+  console.log(
+    `John's BMI (${johnBmi1}) is higher than Marks's (${Math.round(marksBmi1)})`
+  );
+}
